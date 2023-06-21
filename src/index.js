c58690637b1d0f29fcea99bee31d8d837093248d
KULDEEP SINGH NEGI <nkuldeep615@gmail.com>
2023-06-21 13:56:58 +0530

    update_media_queary

--- src/index.js
+++ src/index.js
@@ -1,23 +1,75 @@
-var typeData = new Typed(".role", {
-    strings: [
-      "Full Stack Web Developer",
-      "NodeJs Backend Developer",
-      "Coder",
-    ],
-    loop: true,
-    typeSpeed: 100,
-    backSpeed: 80,
-    backDelay: 1000,
+$(document).ready(function(){
+  $(window).scroll(function(){
+      // sticky navbar on scroll script
+      if(this.scrollY > 20){
+          $('.navbar').addClass("sticky");
+      }else{
+          $('.navbar').removeClass("sticky");
+      }
+      
+      // scroll-up button show/hide script
+      if(this.scrollY > 500){
+          $('.scroll-up-btn').addClass("show");
+      }else{
+          $('.scroll-up-btn').removeClass("show");
+      }
   });
 
+  // slide-up script
+  $('.scroll-up-btn').click(function(){
+      $('html').animate({scrollTop: 0});
+      // removing smooth scroll on slide-up button click
+      $('html').css("scrollBehavior", "auto");
+  });
 
-  GitHubCalendar(".calendar", "kuldeepkd13");
-  GitHubCalendar(".calendar", "kuldeepkd13", { responsive: true });
+  $('.navbar .menu li a').click(function(){
+      // applying again smooth scroll on menu items click
+      $('html').css("scrollBehavior", "smooth");
+  });
 
+  // toggle menu/navbar script
+  $('.menu-btn').click(function(){
+      $('.navbar .menu').toggleClass("active");
+      $('.menu-btn i').toggleClass("active");
+  });
 
+  // typing text animation script
+  // var typed = new Typed(".typing", {
+  //     strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
+  //     typeSpeed: 100,
+  //     backSpeed: 60,
+  //     loop: true
+  // });
 
+  var typed = new Typed(".typing-2", {
+      strings: ["Full Stack Web Developer",
+      "NodeJs Backend Developer",
+      "Coder"],
+      typeSpeed: 100,
+      backSpeed: 60,
+      loop: true
+  });
 
-  var runredirect = function () {
-    var redirectw1 = window.open('https://drive.google.com/file/d/16JrcbczRe513_FSivXWaQdbFUyytkddO/view?usp=share_link');
-    redirectw1.location;
-}
+  // owl carousel script
+  $('.carousel').owlCarousel({
+      margin: 20,
+      loop: true,
+      autoplay: true,
+      autoplayTimeOut: 2000,
+      autoplayHoverPause: true,
+      responsive: {
+          0:{
+              items: 1,
+              nav: false
+          },
+          600:{
+              items: 2,
+              nav: false
+          },
+          1000:{
+              items: 3,
+              nav: false
+          }
+      }
+  });
+});
